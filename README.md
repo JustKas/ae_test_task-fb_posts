0e5656b6f7ec0646eb64fa06ccaa31ed070c8f04
justkas <aleksandr.kasianov.87@gmail.com>
2018-12-14 11:39:56 +0200

    Add project data

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # ae_test_task-fb_posts
+#### COMMON DEPENDENCIES:
+* python 3.6.5
+
+#### FOR SETUP PYTHON DEPENDENCIES:
+For unix OS:
+* Create and activate virtualenv
+> pip3 install virtualenv
+* Verify installation is successful
+> virtualenv --version
+* Create a virtual environment for a project (execute command from the project directory)
+* Go to the project directory
+> virtualenv venv
+* For activation virtualenv (execute command from the project directory)
+> source venv/bin/activate
+* Setup python dependencies (execute command from the project directory)
+> pip3 install -r requirements.txt
+
+### PAY ATTENTION!!!
+* You should update all requirement variables (see "{project_path}/config.py")
+
+
+##### FAQ (How to run the tests)
+* For run all tests
+> pytest
---
+++ config.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------------------------------
+# PAY ATTENTION
+# You should update requirement variables (user data): access_token, user_name, user_id.
+
+# This is test user access token
+access_token = "EAAESxieW3GoBANR80X8m4sgVL1vBl3i27Kp6roSh772BijvfuIHOwjc73FTcxdCi2XAybWHNDeqltmH30ENIqA1Jk7aO2tPj8f7ys0cMPHuKEhAbp8ZA9AxVr2Ox8V5EpvBmqKqzHMdUT5zgCvVNw5BbCjZBLkv1dHE2dZCLqxWtSRm9UBZBv1cZApGjOq523M2QOHoPOZCChaZBmZBJzXYsRACrZCRkJPVIvJoagm713wwZDZD"
+
+# This is test user name (const)
+user_name = "Mark Alcajidgddcea Wongson"
+
+# This is test user ID (const)
+user_id = "106047217108436"
+
+# -----------------------------------------------------------------------------------
+
+# This is base URL for FB graph API
+base_url = "https://graph.facebook.com/v3.2"
+
+# This test message text
+message = 'Enjoy it!'
+
+outdated_message = "Sure"
+
+updated_message = "TYVM"
+
+# This is const profile date
+profile_name = 'Mate'
+profile_picture_url = 'http://www.theoldrobots.com/images14/robo47.JPG'
+
+# -----------------------------------------------------------------------------------
